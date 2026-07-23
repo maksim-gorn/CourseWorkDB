fd5de27bfee9fdd0b864b5bbfe839e534906c5fc
isakahram <ismail.kahramanov@gmail.com>
2026-07-24 02:02:35 +0700

    refacotred coments and added reamde.md with launch instructions

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os
 
+# путь к папке с проектом
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# путь к файлу sqlite базы данных
 DB_PATH = os.path.join(os.path.dirname(BASE_DIR), 'shop_manager.db')
 
 
